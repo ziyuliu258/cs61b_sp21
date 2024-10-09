e9fc7ead3d8e117d06cce35447f9b236ed439381
ziyuliu258 <1209830516abc@gmail.com>
2024-10-09 21:05:29 +0800

    lab 02 IntListExercises little revision

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,7 +14,8 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
-        head.first+=c;
+        if(lst.size()>0)
+            head.first+=c;
     }
 
     /**
