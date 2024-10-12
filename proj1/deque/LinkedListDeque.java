57d34da39ebaa19ccbf5240d759c629ae735978b
ziyuliu258 <1209830516abc@gmail.com>
2024-10-12 12:42:07 +0800

    built a test file to check javac function, and finished a little bit of proj1.

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,73 @@
+package deque;
+
+public class LinkedListDeque <T> {
+    private DNode<T> sentinel;
+    private DNode<T> last;
+    public int Lsize;
+    public LinkedListDeque(){
+        Lsize=0;
+        sentinel=new DNode<>(null,null,null);
+        last=new DNode<>(null,null,null);
+        sentinel.next=sentinel;
+        sentinel.prev=sentinel;
+        last=sentinel;
+    }
+    public int size(){
+        return Lsize;
+    }
+
+    /*There might be something wrong with add methods,but I currently can't figure it out.*/
+    public void addFirst(T elem){
+        sentinel.next=new DNode<>(elem,sentinel,sentinel.next);
+        Lsize++;
+    }
+    public void addLast(T elem){
+        last.prev=new DNode<>(elem,last.prev,last);
+        Lsize++;
+    }
+
+    public T removeLast(){
+        if(size()==0)
+            return null;
+        DNode<T> tmp=last;
+        last=last.prev;
+        last.next=sentinel;
+        sentinel.prev=last;//that is an easy-to-forget point!
+        Lsize--;
+        return tmp.data;
+    }
+    public T removeFirst(){
+        if(size()==0){
+            return null;
+        }
+        else{
+            DNode<T> tmp=sentinel.next;
+            sentinel.next=sentinel.next.next;
+            return tmp.data;
+        }
+    }
+
+    public boolean isEmpty(){
+        return (size()==0);//or (size()==0?true:false); a more complex version.
+    }
+
+    public void printDeque(){
+        if(size()==0)
+            return;
+        for(DNode<T> i=sentinel.next;i!=sentinel;i=i.next){
+            System.out.println(i.data.toString());
+        }
+    }
+
+
+}
+class DNode<T> {
+    T data;
+    DNode<T> next;
+    DNode<T> prev;
+    public DNode(T elem,DNode<T> P,DNode<T> N){
+        data=elem;
+        prev=P;
+        next=N;
+    }
+}//HELPER CLASS REQUIRED
